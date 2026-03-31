9ae9ed18b9c0574dcce081d5bf2dc9eb67ef7eb7
joão pedro <vidaljoao1994@gmail.com>
2026-03-30 21:39:15 -0300

    criado o models e suas criações

--- mural/admin.py
+++ mural/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Categoria, Aviso
 
-# Register your models here.
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo')
+    search_fields = ('titulo',)
+    ordering = ('titulo',)
+
+
+@admin.register(Aviso)
+class AvisoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo', 'categoria', 'data_criacao')
+    list_filter = ('categoria', 'data_criacao')
+    search_fields = ('titulo', 'conteudo')
+    ordering = ('-data_criacao',)
+    date_hierarchy = ('data_criacao')
--- mural/models.py
+++ mural/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    titulo = models.CharField(max_length=50, unique=True)
+
+class Aviso(models.Model):
+    titulo = models.CharField(max_length=100) 
+    conteudo = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    data_criacao = models.DateTimeField(auto_now_add=True) 
